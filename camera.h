17d9154061d724b7a5b1cec3118cdc474090e9a0
sqrout <xucanthwcdhql@outlook.com>
2026-08-28 14:46:36 +0800

    Optimization: limit the number of child rays, fix the shadow acnes, add lambertian reflection, gamma correction.

--- camera.h
+++ camera.h
@@ -11,6 +11,7 @@ public:
     double aspect_ratio = 16.0 / 9.0;
     double img_width = 400;
     int samples_per_pixel = 100;
+    int max_depth = 50;
 
     void render(const hittable& world) {
         initialize();
@@ -27,7 +28,7 @@ public:
                 color pixel_color(0, 0, 0);
                 for (int k = 0; k < samples_per_pixel; k++) {
                     ray r = get_ray(x, y);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(fout, pixel_samples_scale * pixel_color);
             }
@@ -79,11 +80,13 @@ private:
         // [-0.5, 0.5)
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
+        if (depth < 0) return color(0, 0, 0);
         hit_record rec;
-        if (world.hit(r, interval(0, infinity), rec)) {
-            vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+        if (world.hit(r, interval(0.001, infinity), rec)) {
+            // the simulation of lambertian reflection
+            vec3 direction = rec.normal + random_unit_vector();
+            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
             // return 0.5 * (rec.normal + color(1, 1, 1));
         }
         vec3 unit_direction = unit_vector(r.direction());
--- color.h
+++ color.h
@@ -6,11 +6,23 @@
 
 using color = vec3;
 
+// gamma correction(simply)
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0) {
+        return std::sqrt(linear_component);
+    }
+    return 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
     // Normalization
     double r = pixel_color.x();
     double g = pixel_color.y();
     double b = pixel_color.z();
+
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     interval intensity(0.000, 0.999);
     int rByte = int(256 * intensity.clamp(r));
